aacbb25824a9bd4e358b09618b1ca01b1d804757
Akshay Ramesh <akshayrameshbabu@gmail.com>
2019-12-23 20:53:25 +0530

    cos theta fix

--- scripts/entities/collisionEngine/index.js
+++ scripts/entities/collisionEngine/index.js
@@ -12,7 +12,7 @@ export default class CollisionEngine {
 				const { x: x2, y: y2 } = foodCoordinate
 				const distance = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
 				if(distance < monster.senses && (!monster.target || monster.target.distance > distance)){
-					const angle = Math.trunc(Math.asin((x2 - x1)/distance) * (180 / Math.PI))
+					const direction = Math.trunc(Math.acos((x2 - x1)/distance) * (180 / Math.PI))
 					monster.target = { x: x2, y: y2, distance }
 				}
 			})
